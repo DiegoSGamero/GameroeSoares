21164a01420a92f36e1d72dabda969dbe4383296
diego joao <diegosgjoao@gmail.com>
2023-11-07 18:32:04 -0300

    Contrator model created

---
+++ db/migrate/20231107212944_create_contractors.rb
@@ -0,0 +1,16 @@
+class CreateContractors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contractors do |t|
+      t.string :full_name
+      t.string :RG
+      t.string :CPF_CNPJ
+      t.string :fantasy_name
+      t.string :address
+      t.string :phone_number
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
